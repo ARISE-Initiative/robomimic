5fc1e0e1157190a934911d8a4224ac019d9404a1
snasiriany <snasiriany@gmail.com>
2021-12-20 09:37:19 -0600

    limit num torch threads

--- robomimic/scripts/train.py
+++ robomimic/scripts/train.py
@@ -51,8 +51,7 @@ def train(config, device):
     np.random.seed(config.train.seed)
     torch.manual_seed(config.train.seed)
 
-    # set num workers
-    torch.set_num_threads(1)
+    torch.set_num_threads(2)
 
     print("\n============= New Training Run with Config =============")
     print(config)
