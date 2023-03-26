c98459bade5e58d8425e809031b29f5140238661
Danfei Xu <danfeix@nvidia.com>
2023-03-26 11:52:15 -0400

    randomizer visualization

---
+++ robomimic/utils/macros.py
@@ -0,0 +1,9 @@
+"""
+Set of global variables shared across robomimic
+"""
+# Sets debugging mode. Should be set at top-level script so that internal
+# debugging functionalities are made active
+DEBUG = False
+
+# Whether to visualize the before & after of an observation randomizer
+VISUALIZE_RANDOMIZER = True
