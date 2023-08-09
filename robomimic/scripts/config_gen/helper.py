d191107f7772a41a7a4b11ee7d384ca4a1289608
snasiriany <snasiriany@gmail.com>
2023-08-09 14:57:26 -0500

    save checkpoints for r2d2 every 50 epochs

--- robomimic/scripts/config_gen/helper.py
+++ robomimic/scripts/config_gen/helper.py
@@ -59,6 +59,12 @@ def set_env_settings(generator, args):
                 False
             ],
         )
+        generator.add_param(
+            key="experiment.save.every_n_epochs",
+            name="",
+            group=-1,
+            values=[50],
+        )
         if "observation.modalities.obs.low_dim" not in generator.parameters:
             generator.add_param(
                 key="observation.modalities.obs.low_dim",
