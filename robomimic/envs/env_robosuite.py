57fd17c5ed2541c562c689e4c834f403c8b00e51
j96w <j96w@qq.com>
2023-09-11 21:47:29 -0700

    libero reset from xml

--- robomimic/envs/env_robosuite.py
+++ robomimic/envs/env_robosuite.py
@@ -11,6 +11,7 @@ import robosuite
 
 import robomimic.utils.obs_utils as ObsUtils
 import robomimic.envs.env_base as EB
+from libero.libero.utils.utils import postprocess_model_xml
 
 # protect against missing mujoco-py module, since robosuite might be using mujoco-py or DM backend
 try:
@@ -136,15 +137,10 @@ class EnvRobosuite(EB.EnvBase):
         should_ret = False
         if "model" in state:
             self.reset()
-            # ----- LIBERO does not require xml edit ----
-            # robosuite_version_id = int(robosuite.__version__.split(".")[1])
-            # if robosuite_version_id <= 3:
-            #     from robosuite.utils.mjcf_utils import postprocess_model_xml
-            #     xml = postprocess_model_xml(state["model"])
-            # else:
-            #     # v1.4 and above use the class-based edit_model_xml function
-            #     xml = self.env.edit_model_xml(state["model"])
-            # self.env.reset_from_xml_string(xml)
+            # ----- loading LIBERO model xml ----
+            model_xml = state["model"]
+            model_xml = postprocess_model_xml(model_xml, {})
+            self.env.reset_from_xml_string(model_xml)
             self.env.sim.reset()
             if not self._is_v1:
                 # hide teleop visualization after restoring from model
