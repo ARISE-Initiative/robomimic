52489eadfe872ffe92c39108379122063c874c82
Ajay Mandlekar <ajay.mandlekar@gmail.com>
2023-10-19 18:50:51 -0700

    modify docstring for env robosuite

--- robomimic/envs/env_robosuite.py
+++ robomimic/envs/env_robosuite.py
@@ -52,6 +52,10 @@ class EnvRobosuite(EB.EnvBase):
                 on every env.step call. Set this to False for efficiency reasons, if image
                 observations are not required.
 
+            use_depth_obs (bool): if True, environment is expected to render depth image observations
+                on every env.step call. Set this to False for efficiency reasons, if depth
+                observations are not required.
+
             postprocess_visual_obs (bool): if True, postprocess image observations
                 to prepare for learning. This should only be False when extracting observations
                 for saving to a dataset (to save space on RGB images for example).
