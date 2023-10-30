6d435fc51c93c1d2df04c19462566f19549c0620
Yixuan Wang <yixuan22@illinois.edu>
2023-10-30 15:14:36 -0500

    add pointnet

--- robomimic/config/base_config.py
+++ robomimic/config/base_config.py
@@ -272,6 +272,14 @@ class BaseConfig(Config):
         self.observation.encoder.scan.core_kwargs = Config()                    # See models/obs_core.py for important kwargs to set and defaults used
         self.observation.encoder.scan.core_kwargs.do_not_lock_keys()
 
+        # =============== Spatial default encoder (pointnet) ===============
+        self.observation.encoder.spatial = deepcopy(self.observation.encoder.rgb)
+
+        # Scan: Modify the core class + kwargs, otherwise, is same as rgb encoder
+        self.observation.encoder.spatial.core_class = "SpatialCore"                   # Default ScanCore class uses Conv1D to process this modality
+        self.observation.encoder.scan.core_kwargs = Config()                    # See models/obs_core.py for important kwargs to set and defaults used
+        self.observation.encoder.scan.core_kwargs.do_not_lock_keys()
+
     def meta_config(self):
         """
         This function populates the `config.meta` attribute of the config. This portion of the config 
