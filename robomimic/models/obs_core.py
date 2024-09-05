29d6ca229dec3327f87b54cf1688a94177f92af6
Arhan Jain <arhan.j04@gmail.com>
2024-09-05 10:26:40 -0700

    Modules registry for supporting custom backbone networks (#190)
    * Modules registry for extending backbones available
    
    * rollback module change, intialize using EncoderCore registry
    
    * cleanup

--- robomimic/models/obs_core.py
+++ robomimic/models/obs_core.py
@@ -34,6 +34,7 @@ class EncoderCore(BaseNets.Module):
     """
     Abstract class used to categorize all cores used to encode observations
     """
+
     def __init__(self, input_shape):
         self.input_shape = input_shape
         super(EncoderCore, self).__init__()
@@ -97,7 +98,9 @@ class VisualCore(EncoderCore, BaseNets.ConvBase):
         backbone_kwargs["input_channel"] = input_shape[0]
 
         # extract only relevant kwargs for this specific backbone
-        backbone_kwargs = extract_class_init_kwargs_from_dict(cls=eval(backbone_class), dic=backbone_kwargs, copy=True)
+        backbone_kwargs = extract_class_init_kwargs_from_dict(
+                cls = ObsUtils.OBS_ENCODER_CORES[backbone_class],
+                dic=backbone_kwargs, copy=True)
 
         # visual backbone
         assert isinstance(backbone_class, str)
